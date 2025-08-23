6f4ded580c4eddd2f3549bbbd446181a6845050a
Dangcaominh <minh.dangrandomname@hcmut.edu.vn>
2025-08-23 07:20:48 +0000

    New fork

--- samples/hello_world/src/main.c
+++ samples/hello_world/src/main.c
@@ -4,11 +4,30 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#include "zephyr/devicetree.h"
 #include <stdio.h>
+#include <stdint.h>
+#include <zephyr/kernel.h>
+#include <zephyr/shell/shell.h>
+
+void print()
+{
+	printk("Hello World\n");
+}
 
 int main(void)
 {
-	printf("Hello World! %s\n", CONFIG_BOARD_TARGET);
+	// struct device* dev = DEVICE_DT_GET(DT_ALIAS(serial1));
+	uint32_t *p = (uint32_t *)(0x20000000);
+	while (1)
+	{
+		*p = 0;
+		k_sleep(K_MSEC(500));
+		*p = 0xf;
+		k_sleep(K_MSEC(500));
+	}
 
 	return 0;
 }
+
+SHELL_CMD_REGISTER(print, NULL, "Print hello world", print);
